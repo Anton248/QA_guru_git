2c76d89f01f9663affbd1afc9bc7125d1b2bfc33
Anton Nikolski <ant.nikolski@gmail.com>
2023-04-02 14:54:41 +0300

    added msg2

--- src/main/java/ru/nikolski/Main.java
+++ src/main/java/ru/nikolski/Main.java
@@ -2,6 +2,7 @@ package ru.nikolski;
 
 public class Main {
     static String msg = "Hello QA GURU!!!!!";
+    static String msg2 = "Hello world";
     public static void main(String[] args) {
         System.out.println(message());
     }
@@ -9,4 +10,5 @@ public class Main {
     public static String message(){
         return msg;
     }
+
 }
