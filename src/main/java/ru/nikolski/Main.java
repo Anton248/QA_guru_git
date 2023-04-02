6fbdf356ed94aea0e8f84cea786565f55c9506ac
Anton Nikolski <31551544+Anton248@users.noreply.github.com>
2023-04-02 14:29:13 +0300

    Update Main.java

--- src/main/java/ru/nikolski/Main.java
+++ src/main/java/ru/nikolski/Main.java
@@ -1,7 +1,8 @@
 package ru.nikolski;
 
 public class Main {
+    static String msg = "Hello QA GURU!";
     public static void main(String[] args) {
-        System.out.println("Hello QA GURU!");
+        System.out.println(msg);
     }
 }
