7f6e4748dae0ae60a80377675e1976529ea163be
Anton Nikolski <ant.nikolski@gmail.com>
2023-04-02 14:33:05 +0300

    Hello Message to dedicated method

--- src/main/java/ru/nikolski/Main.java
+++ src/main/java/ru/nikolski/Main.java
@@ -2,6 +2,11 @@ package ru.nikolski;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA GURU!");
+        System.out.println(message());
+    }
+
+
+    public static String message(){
+        return "Hello QA GURU!";
     }
 }
